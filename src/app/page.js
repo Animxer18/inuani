8d7645f55799913348e889d217367caf4b7a6c23
Animxer18 <138975524+Animxer18@users.noreply.github.com>
2023-10-03 13:58:01 +0800

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,11 @@ import Link from 'next/link';
 
 export default async function Page() {
   async function getAnimeData() {
-    const res = await fetch(`https://api.consumet.org/popular`, { cache: 'no-store' });
+    const res = await fetch(`https://api.consumet.org/anime/gogoanime/popular`, { cache: 'no-store' });
     return res.json();
   }
   async function getRecentAnimeData() {
-    const res = await fetch('https://api.consumet.org/recent', { cache: 'no-store' });
+    const res = await fetch('https://api.consumet.org/anime/gogoanime/recent', { cache: 'no-store' });
     return res.json();
   }
 
@@ -17,10 +17,10 @@ export default async function Page() {
     <div id="front">
       <div id="animegrid">
         {data.data.map((anime) => (
-          <Link href={`/anime/${anime.id}`} key={anime.id}>
-            <div id="anime">
+          <Link href={`/anime/${gogoanime.id}`} key={anime.id}>
+            <div id="gogoanime">
               <img width="250" height="350" alt={anime.title.romaji} src={anime.coverImage} />
-              <h2 style={{ color: anime.color }}>{anime.title.romaji}</h2>
+              <h2 style={{ color: gogoanime.color }}>{gogoanime.title.romaji}</h2>
             </div>
           </Link>
         ))}
@@ -33,9 +33,9 @@ export default async function Page() {
               !recent.image ? null : (
                 <div className="episode" key={recent.Id}>
                   <Link href={`/watch/${recent.animeId}/${recent.id}`}>
-                    <div id="anime">
+                    <div id="gogoanime">
                       <img width="270" height="150" alt={recent.title} src={recent.image} />
-                      <h2 style={{ color: recent.anime.color, fontSize: 24, marginBottom: 1 }}>{recent.anime.title.romaji}</h2>
+                      <h2 style={{ color: recent.anime.color, fontSize: 24, marginBottom: 1 }}>{recent.gogoanime.title.romaji}</h2>
                       <h2 style={{ color: recent.anime.color, fontSize: 22, marginTop: 1 }}>{recent.title}</h2>
                     </div>
                   </Link>
